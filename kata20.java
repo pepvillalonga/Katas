11c103879e9d7a8b61187ea5066b0345ef093c68
Pep <info@pepvillalonga.es>
2025-10-14 17:58:38 +0200

    Arreglado

--- kata20.java
+++ kata20.java
@@ -2,18 +2,18 @@
 public class kata20 {
 
     public static int findIt(int[] a) {
-        for (int i = 0; i < a.length; i++) {
-            int numActual = a[i];
+        for (int letSelec : a) {
             boolean bool = false;
             for (int num : a) {
-                if (num == numActual) {
+                if (num == letSelec) {
                     bool = !bool;
                 }
             }
             if (bool) {
-                return numActual;
+                return letSelec;
             }
         }
         return 0;
     }
+
 }
