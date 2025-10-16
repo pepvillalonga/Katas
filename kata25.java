edeed312a08c0b06906d2ec4076a321a449e1a46
Pep <info@pepvillalonga.es>
2025-10-16 18:06:15 +0200

    kata25

---
+++ kata25.java
@@ -0,0 +1,14 @@
+
+public class kata25 {
+
+    public static void main() {
+        int n = 123456789;
+        System.out.println(solution(n));
+    }
+
+    public static int solution(int n) {
+        String girado = new StringBuilder(String.valueOf(n)).reverse().toString();
+        return Integer.parseInt(girado);
+    }
+
+}
