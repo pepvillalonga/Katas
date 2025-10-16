32fa7ce6e0b1f89994188741f0b8ef7aecb71cc8
Pep <info@pepvillalonga.es>
2025-10-16 20:59:45 +0200

    1

--- kata29.java
+++ kata29.java
@@ -2,7 +2,7 @@
 public class kata29 {
 
     public static boolean isLove(final int flower1, final int flower2) {
-        return (flower1 % 2 == 0 && flower2 % 2 != 0) || (flower1 % 2 != 0 && flower2 % 2 == 0) ;
+        return (flower1 % 2 == 0 && flower2 % 2 != 0) || (flower1 % 2 != 0 && flower2 % 2 == 0);
     }
 
 }
